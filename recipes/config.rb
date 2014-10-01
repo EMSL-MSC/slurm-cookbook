8179737358399dd6826d8704d7108e2c7f38f13f
David ML Brown Jr <david.brown@pnnl.gov>
2014-09-30 22:54:10 -0700

    I think this makes a slurm.conf

--- recipes/config.rb
+++ recipes/config.rb
@@ -1,5 +1,27 @@
 include_recipe "slurm::install"
 
+node_lines = []
+node['slurm']['slurm']['nodes'].each do |line_hash|
+  line = ""
+  line_hash.keys.each do |key|
+    if not line_hash[key].nil?
+      line += key + "=" + line_hash[key] + " "
+    end
+  end
+  node_lines.push(line)
+end
+
+partition_lines = []
+node['slurm']['slurm']['partitions'].each do |part_hash|
+  part = ""
+  part_hash.keys.each do |key|
+    if not part_hash[key].nil?
+      part = key + "=" + part_hash[key] + " "
+    end
+  end
+  partition_lines.push(part)
+end
+
 template "slurm_conf" do
   path node['slurm']['configdir']+"/slurm.conf"
   owner "root"
@@ -7,8 +29,8 @@ template "slurm_conf" do
   mode "0644"
   variables(
     :config => node['slurm']['slurm']['config'],
-    :nodes => node['slurm']['slurm']['nodes'],
-    :partitions => node['slurm']['slurm']['partitions']
+    :nodes => node_lines,
+    :partitions => partition_lines
   )
 end
 
